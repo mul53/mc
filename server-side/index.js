af1635c512a5d81a5e09c0141ff81cba7c703f36
mul53 <mulengabowa53@gmail.com>
2019-03-16 12:06:11 +0200

    Added express.js Added debug.js and morgan for logging Set up express server Added eslint and prettier code quality

--- server-side/index.js
+++ server-side/index.js
@@ -1,1 +1,34 @@
-// index
+var express = require('express');
+var logger = require('morgan');
+var http = require('http');
+var debug = require('debug')('app:server');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.set('port', port);
+
+app.use(logger('dev'));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+var server = http.createServer(app);
+
+server.listen(port);
+server.on('listening', onListening);
+server.on('error', onError);
+
+function onListening() {
+  debug('Listening on port ' + app.get('port'));
+}
+
+function onError(err) {
+  if (err.code === 'EADDRINUSE') {
+    process.stdout.write(`Port ${err.port} is already in use.`);
+  } else {
+    process.stdout.write(err.code);
+  }
+}
