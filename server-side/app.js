cad0b775b677cbe7575588b9e41cde327884552e
mul53 <mulengabowa53@gmail.com>
2019-03-19 14:18:06 +0200

    added cors config

--- server-side/app.js
+++ server-side/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var apiV1Router = require('./routes/v1');
 
@@ -8,6 +9,7 @@ var app = express();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/api/v1', apiV1Router);
 
